7e84a7fa8790a5a12d5a0a821806d07adbd7ff29
puthpiseth <puthpiseth_tun@yahoo.com>
2021-05-04 00:29:12 +0200

    Add favorite icon

--- src/utils/componentsUtils/Card.jsx
+++ src/utils/componentsUtils/Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../assets/styleSheets/Card.scss';
 import StarIcon from '@material-ui/icons/Star';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 
 function Card({img, name, description, price_by_night}) {
     return (
         <figure className="card">
+        <FavoriteIcon className="fav-icon"/>
             <img src={img} />
             <figcaption>
                 
